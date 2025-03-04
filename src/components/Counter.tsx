4819ae61edcb41b9ce2285aa9a2178127bb07014
Davi Silva <davisilvaphoto@gmail.com>
2025-03-04 14:46:12 -0300

    some life cycle components

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,32 +1,53 @@
-import React, { ReactNode } from "react";
-import { Button } from "./ui/button";
-import { Card } from "./Card";
+import React, { ReactNode } from 'react'
+import { Button } from './ui/button'
+import { Card } from './Card'
 
 interface IProps {
-  title: string;
+  title: string
 }
 
 interface IState {
-  counter: number;
-  isLoading: boolean;
+  counter: number
+  isLoading: boolean
 }
 
 export class Counter extends React.Component<IProps, IState> {
   state: Readonly<IState> = {
     counter: 0,
     isLoading: false,
-  };
+  }
 
   handleChangeCounter(value: 1 | -1) {
-    this.setState({ counter: this.state.counter + value });
+    this.setState({ counter: this.state.counter + value })
   }
 
   async componentDidMount(): Promise<void> {
-    this.setState({ isLoading: true });
+    this.setState({ isLoading: true })
 
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    window.addEventListener('resize', this.handleResize)
 
-    this.setState({ isLoading: false });
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    this.setState({ isLoading: false })
+  }
+
+  componentDidUpdate(_: Readonly<IProps>, prevState: Readonly<IState>): void {
+    console.log({
+      previous: prevState,
+      current: this.state,
+    })
+  }
+
+  handleResize() {
+    console.log('window resized...')
+  }
+
+  componentWillUnmount(): void {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+    console.log({ error, errorInfo })
   }
 
   render(): ReactNode {
@@ -39,6 +60,6 @@ export class Counter extends React.Component<IProps, IState> {
           <Button onClick={() => this.handleChangeCounter(-1)}>-</Button>
         </div>
       </Card>
-    );
+    )
   }
 }
