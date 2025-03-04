d9793014b975e2fc28e8c0cdeae2c934f4e7c0ed
Davi Silva <davisilvaphoto@gmail.com>
2025-03-04 07:45:49 -0300

    initial files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+
+export const App = () => (
+  <div className="min-h-screen grid place-items-center">
+    <div>
+      <Input />
+      <Button>Hello JStack</Button>
+    </div>
+  </div>
+)
